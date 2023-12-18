2d9407b590f57366224dc0e41e0337cfc47dd956
Ketema Girma <ketema.girma@a2sv.org>
2023-12-18 23:20:55 +0300

    add auth page

---
+++ lib/src/auth/register_or_login.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/src/login_page.dart';
+import 'package:social_media_app/src/register_page.dart';
+
+class AuthenticationPage extends StatefulWidget {
+  const AuthenticationPage({super.key});
+
+  @override
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
+}
+
+class _AuthenticationPageState extends State<AuthenticationPage> {
+  bool showLoginPage = true;
+
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
