38c839ca2e50dd7db5090c33f04e88c747a36e03
Ketema Girma <ketema.girma@a2sv.org>
2023-12-20 14:41:27 +0300

    add drawer

---
+++ lib/src/widgets/drawer.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    "A 2 S V",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.home,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                  title: const Text("H O M E"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/home-page");
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.person,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                  title: const Text("P R O F I L E"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/profile-page");
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.dashboard,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                  title: const Text(" D A S H O R D"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/users-page");
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(Icons.group,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                  title: const Text(" S T U D E N T S"),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/users-page");
+                  },
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, bottom: 25),
+            child: ListTile(
+              leading: Icon(Icons.logout,
+                  color: Theme.of(context).colorScheme.inversePrimary),
+              title: const Text("L O G O U T"),
+              onTap: () {
+                Navigator.pop(context);
+                logout();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
