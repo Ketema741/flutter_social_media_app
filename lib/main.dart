859b39e6445026e85f95749454e2c268d9bd7af3
Ketema Girma <ketema.girma@a2sv.org>
2023-12-20 14:42:01 +0300

    add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_app/home_page.dart';
+import 'package:social_media_app/src/auth/auth.dart';
 import 'package:social_media_app/src/auth/register_or_login.dart';
 
+import 'firebase_options.dart';
+import 'src/profile_page.dart';
 import 'src/theme/dark_mode.dart';
 import 'src/theme/light_mode.dart';
+import 'src/users_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -19,7 +27,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       darkTheme: darkMode,
-      home: const AuthenticationPage(),
+      home: const AuthPage(),
+      routes: {
+        "/login-page": (context) => const AuthenticationPage(),
+        "/home-page": (context) => const HomePage(),
+        "/profile-page": (context) => const ProfilePage(),
+        "/users-page": (context) => const UsersPage(),
+      },
     );
   }
 }
